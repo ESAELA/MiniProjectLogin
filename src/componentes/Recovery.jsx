f0d057a6226a3e8897926c374588d92c6bca722c
ESAELA <esamuelchinama@hotmail.com>
2025-03-10 22:06:04 +0200

    recovery finished

--- src/componentes/Recovery.jsx
+++ src/componentes/Recovery.jsx
@@ -9,21 +9,21 @@ function Recovery () {
 
                 <div className="max-w-sm flex flex-col gap-5 mt-20 xl:flex-col xl:justify-center xl:items-center md:items-center md:flex-col">
 
-                    <div className="p-10 text-white space-y-8 xl:w-6xl md:w-3xl">
-                        <div className=''>
-                            <h1 className="text-4xl font-semibold text-center uppercase md:text-7xl md:leading-20 l:text-7xl xl:leading-10 -tracking-tighter "> 
+                    <div className="p-10 text-white space-y-8 xl:w-6xl md:w-3xl xl:flex xl:flex-col xl:items-center xl:justify-center">
+                        <div className='xl:w-5xl xl:h-20 xl:flex xl:items-center xl:justify-center'>
+                            <h1 className="text-4xl font-semibold text-center uppercase md:text-7xl md:leading-20 l:text-7xl xl:leading-10 -tracking-tighter"> 
                                 Não consegue entrar?
                             </h1>
                         </div>
                         <div className='xl:w-3xl'>
-                            <p className="text-xl text-center md:text-4xl xl:text-2xl tracking-[2px]">
+                            <p className="text-xl text-center md:text-4xl  xl:text-2xl tracking-[2px]">
                                 Há alguns motivos pelos quais você pode não conseguir fazer login. Verifique as opções abaixo para possíveis soluções.
                             </p>
                         </div>
                     </div>
             
 
-                    <div className='xl:flex xl:space-x-10 md:w-3xl xl:justify-center space-y-10 xl:space-y-0 xl:w-6xl '>
+                    <div className='xl:flex xl:space-x-10 md:w-3xl xl:justify-center space-y-10 xl:space-y-0 xl:w-6xl'>
 
                     <div className="p-13 bg-white flex flex-col items-center  transition duration-200 ease-in-out hover:-translate-y-1 cursor-pointer">
                         <div className='mb-5'>
