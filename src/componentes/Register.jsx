3b05bf8a4c7fd8759eb4f352542f47b783c87aa6
ESAELA <esamuelchinama@hotmail.com>
2025-03-10 23:10:17 +0200

    up

--- src/componentes/Register.jsx
+++ src/componentes/Register.jsx
@@ -3,27 +3,29 @@ import { mdiArrowRight } from '@mdi/js';
 
 function Register () {
     return (
-       <div className="bg-blue-300 min-h-screen flex flex-col justify-center items-center">
-            <div>
-                <h1 className="uppercase">Criar uma conta</h1>
+       <div className="bg-blue-300 min-h-screen flex flex-col justify-center items-center font-display">
+            <div className='mb-8'>
+                <h1 className="uppercase text-3xl text-white font-bold">Criar uma conta</h1>
             </div>
-            <div className="bg-blue-200">
+            <div className="bg-white shadow-xl flex flex-col items-center p-10 gap-5">
                 <div>
-                    <h1>
+                    <h1 className='text-black font-bold text-2xl'>
                         Qual é seu e-mail?
                     </h1>
                 </div>
-                <div>
-                    <h2>
+                <div className='text-center width-componentes'>
+                    <h2 className='text-2xl text-gray-500 font-semibold'>
                         Não se preocupe, não contaremos a ninguém.
                     </h2>
                 </div>
-                <input type="email" name="" id="" placeholder="Email" />
-                <div className='rounded-4xl cursor-pointer border-2 border-gray-300 transition duration-300 ease-in-out hover:-translate-y-1'>
+                <div className='bg-gray-100 width-componentes h-15'>
+                    <input type="email" name="" id="" placeholder='E-mail' className='h-full w-full p-5 font-black uppercase'/>
+                </div>
+                <div className='rounded-4xl p-2 px-4 py-4 mt-4 cursor-pointer border-2 border-gray-300 transition duration-300 ease-in-out hover:-translate-y-1'>
                     <Icon path={mdiArrowRight} size={1.4} />
                 </div>
-                <div>
-                    <a href="">
+                <div className=''> 
+                    <a href="" className='text-gray-500 text-md tracking-wider font-bold hover:text-black'>
                         Já tem uma conta?
                     </a>
                 </div>
