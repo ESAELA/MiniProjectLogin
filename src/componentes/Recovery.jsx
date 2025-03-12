0d5306bd5f662c70fbac22effd75b3f87866412c
ESAELA <esamuelchinama@hotmail.com>
2025-03-12 13:57:03 +0200

    up

--- src/componentes/Recovery.jsx
+++ src/componentes/Recovery.jsx
@@ -7,7 +7,7 @@ function Recovery () {
         <div className="bg-[url(src/img/bg-recovery.jpg)] bg-center bg-cover bg-no-repeat min-h-screen">
             <div className="flex flex-col justify-center items-center font-display">
 
-                <div className="max-w-sm flex flex-col gap-5 mt-20 xl:flex-col xl:justify-center xl:items-center md:items-center md:flex-col">
+                <div className="max-w-sm flex flex-col gap-5 mt-20 xl:flex-col xl:justify-center xl:items-center md:items-center md:flex-col sm:flex sm:flex-col sm:justify-center sm:items-center">
 
                     <div className="p-10 text-white space-y-8 xl:w-6xl md:w-3xl xl:flex xl:flex-col xl:items-center xl:justify-center sm:flex sm:flex-col sm:justify-center sm:items-center">
                         <div className='xl:w-5xl xl:h-20 xl:flex xl:items-center xl:justify-center sm:w-md'>
@@ -22,14 +22,14 @@ function Recovery () {
                         </div>
                     </div>
             
-
-                    <div className='xl:flex xl:space-x-10 md:w-2xl xl:justify-center space-y-10 xl:space-y-0 xl:w-6xl'>
+                    
+                    <div className='xl:flex xl:space-x-10 md:w-2xl xl:justify-center space-y-10 xl:space-y-0 xl:w-6xl sm:w-xl'>
 
                     <a href='forgotUsername' className="p-13 bg-white flex flex-col items-center  transition duration-200 ease-in-out hover:-translate-y-1 cursor-pointer">
                         <div className='mb-5'>
                             <Icon path={mdiAccount} size={5} />
                         </div>
-                        <div className='xl:w-92 xl:h-50 xl:space-y-5 md:space-y-5 md:w-1xl '>
+                        <div className='xl:w-92 xl:h-50 xl:space-y-5 md:space-y-5 md:w-1xl sm:space-y-3'>
                             <h1 className='text-center text-2xl md:text-5xl font-semibold xl:text-4xl'>
                                 Esqueçeu o nome de usuário?
                             </h1>
@@ -42,7 +42,7 @@ function Recovery () {
                         <div className='mb-5'>
                             <Icon path={mdiLockCheck} size={5} />
                         </div>
-                        <div className='xl:w-92 xl:h-50 xl:space-y-5 md:space-y-5 md:w-1xl'>
+                        <div className='xl:w-92 xl:h-50 xl:space-y-5 md:space-y-5 md:w-1xl sm:space-y-3'>
                             <h1 className='text-center text-2xl font-semibold xl:text-3xl md:text-5xl'>
                                 Esqueçeu a sua senha?
                             </h1>
@@ -54,6 +54,7 @@ function Recovery () {
                     </a>
 
                     </div>
+                    
 
                 </div>
             </div>
